9d3e4d0a443c8c175ed263177bc927d5212a0ae9
Anderson <andersondavid2000@gmail.com>
2020-01-14 15:31:01 -0500

    first commit

---
+++ CodePathTippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodePathTippy
+//
+//  Created by Anderson David on 1/14/20.
+//  Copyright © 2020 Anderson David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
